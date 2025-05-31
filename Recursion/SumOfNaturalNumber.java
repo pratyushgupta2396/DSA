2af3f3b5744b4c94c07c47d9f2d290df77e93f77
Pratyush Gupta <164205316+pratyushgupta2396@users.noreply.github.com>
2025-06-01 01:18:19 +0530

    SumOfNaturalNumber.java

---
+++ Recursion/SumOfNaturalNumber.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+	static long NumberSum(long N) {
+        //Write your code here
+        if(N==1){
+            return 1;
+        }
+        return N+NumberSum( N-1);
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long N = scanner.nextLong();
+        System.out.println(NumberSum(N));
+    }
+}
