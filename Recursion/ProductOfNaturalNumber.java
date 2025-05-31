bd664a68fe6b8578a88ca029639cb9b51a5482d5
Pratyush Gupta <164205316+pratyushgupta2396@users.noreply.github.com>
2025-06-01 01:21:14 +0530

    ProductOfNaturalNumber.java

---
+++ Recursion/ProductOfNaturalNumber.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+	static long product(int n){
+		//Write code here
+        if(n==1) return 1;
+
+        return n*product(n-1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        System.out.println(product(n));
+    }
+}
