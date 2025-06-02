47ad5c1bf124d5912b1b56fb3bc8268c26fe4380
Pratyush Gupta <164205316+pratyushgupta2396@users.noreply.github.com>
2025-06-02 23:16:23 +0530

    Implementing Multiplication Using Recursive

--- Recursion/RecursiveMultiplication.java
+++ Recursion/RecursiveMultiplication.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 public class Main {
   public static int multiplyRecursively(int n, int m) {
-    //Write code here and print output
+    //Write code here and print output 
     if(m==0){
       return 0 ;
     }
