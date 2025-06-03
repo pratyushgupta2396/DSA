f37b64ad173231cd733d6cbdc7a9346484d6a440
Pratyush Gupta <164205316+pratyushgupta2396@users.noreply.github.com>
2025-06-03 14:35:31 +0530

    Implementing Delete Node In Linked List

---
+++ Linked Lists/DeleteNodeInLinkedList.java
@@ -0,0 +1,85 @@
+import java.util.*;
+
+class Node
+{
+	int data;
+	Node next;
+	
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}
+
+class Main
+{
+	Node head;
+	void printList(Node head)
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+
+	public void addToTheLast(Node node) 
+	{
+		if (head == null) 
+		{
+			head = node;
+		} else 
+		{
+		   Node temp = head;
+		   while (temp.next != null)
+		   temp = temp.next;
+
+		   temp.next = node;
+		}
+    }
+
+	public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		Main llist = new Main();
+		int a1 = sc.nextInt();
+		Node head = new Node(a1);
+        llist.addToTheLast(head);
+        for (int i = 1; i < n; i++) 
+		{
+			int a = sc.nextInt(); 
+			llist.addToTheLast(new Node(a));
+		}
+		int x = sc.nextInt();
+        Node node = head;
+        while(node!=null)
+        {
+            if(node.data == x)
+            break;
+            node=node.next;
+        }
+		Solution g = new Solution(); 
+		g.deleteNode(node);
+		llist.printList(llist.head);
+	}
+}
+
+class Solution
+{
+    void deleteNode(Node node)
+    {
+	    // Your code here
+		//int val;
+		
+		Node next = node.next;
+        int temp = node.data;
+        node.data = next.data;
+        next.data = temp;
+        node.next = node.next.next;
+	    
+    }
+}
