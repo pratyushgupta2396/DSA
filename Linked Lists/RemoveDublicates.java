7da7303602c66c5fb690901c76be1b708ad8ea6d
Pratyush Gupta <164205316+pratyushgupta2396@users.noreply.github.com>
2025-06-04 21:49:02 +0530

    Implementing Remove Dublicates from Sorted List

---
+++ Linked Lists/RemoveDublicates.java
@@ -0,0 +1,77 @@
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int key) {
+        data = key;
+        next = null;
+    }
+}
+
+class Main {
+    static Node head;
+    static Node temp;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+            int n = sc.nextInt();
+            int a1 = sc.nextInt();
+            head = null;
+            temp = null;
+            addToTheLast(a1);
+
+            for (int i = 1; i < n; i++) {
+                int data = sc.nextInt();
+                addToTheLast(data);
+            }
+            Solution ans = new Solution();
+            Node node = ans.solve(head);
+            printList(node);
+            System.out.println();
+            sc.close();
+    }
+
+    public static void addToTheLast(int data) {
+        if (head == null) {
+            head = new Node(data);
+            temp = head;
+            return;
+        } else {
+            Node new_node = new Node(data);
+            temp.next = new_node;
+            temp = temp.next;
+            return;
+        }
+    }
+
+    public static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+}
+
+class Solution {
+    public Node solve(Node head) {
+        // your code here
+        if(head == null){
+            return null;
+        }
+        Node temp1 = head;
+        Node temp2 = head;
+        while(temp2 != null){
+            if(temp1.data == temp2.data){
+                temp2 = temp2.next;
+            }else{
+                temp1.next = temp2;
+                temp1 = temp2;
+            }
+        }
+        temp1.next = temp2;
+        return head;
+    }
+}
