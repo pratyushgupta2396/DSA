9f52637c3a72ff85989852ece98cb463d926684a
Pratyush Gupta <164205316+pratyushgupta2396@users.noreply.github.com>
2025-06-07 17:15:06 +0530

    Implementing Binary Search

---
+++ Binary Search/BinarySearch.java
@@ -0,0 +1,36 @@
+//Given an sorted integer array of size n which contains unique elements, find the index (0-based) of the given key.
+
+//If the key is not present then return -1.
+
+import java.util.*;
+
+public class Main {
+    public static int findIndex(int key, int[] arr) {
+        //Write code here
+        int low = 0;
+        int high = arr.length - 1;
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(arr[mid] == key){
+                return mid;
+            }else if(arr[mid] < key){
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n, key;
+        n = sc.nextInt();
+        key = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        System.out.println(findIndex(key, arr));
+        sc.close();
+    }
+}
