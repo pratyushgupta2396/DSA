c398c614adebb6d507ec477ccd373e0cdebe8a66
Pratyush Gupta <164205316+pratyushgupta2396@users.noreply.github.com>
2025-06-07 17:37:27 +0530

    Implementing Peak Element

---
+++ Binary Search/PeakElement.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+class Solution{
+    public int findPeak(int arr[], int n)
+    {
+        //Write your code here
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            
+            if (arr[mid] < arr[mid + 1]) {
+                low = mid + 1;
+            } else {
+                
+                high = mid;
+            }
+        }
+        
+        return low;
+    }
+       
+    
+}
+
+class Main{
+	public static void main(String[] args){
+		try (Scanner sc = new Scanner(System.in)) {
+			int t=sc.nextInt();
+			while(t>0){
+				int n=sc.nextInt();
+				int []A=new int[n];
+				for(int i=0;i<n;i++){
+					A[i]=sc.nextInt();
+				}
+				Solution ob =new Solution();
+				int ans = ob.findPeak(A,n);
+                if(ans==0){
+                    if(A[ans]>A[ans+1]){
+                        System.out.println("true");
+                    }
+                    else{
+                       System.out.println("false");    
+                    }
+                }
+                else if(ans==n-1){
+                    if(A[ans]>A[ans-1]){
+                        System.out.println("true");                        
+                    }
+                    else{
+                        System.out.println("false");                        
+                    }
+                }
+                else{
+                    if(A[ans]>A[ans+1] && A[ans]>A[ans-1]){
+                        System.out.println("true");
+                    }
+                    else{
+                        System.out.println("false");                        
+                    }
+                }
+				t--;
+			}
+		}
+	}
+}
