7148e38d510ea5adfc0bdc67f1f8f3752b52523c
Pratyush Gupta <164205316+pratyushgupta2396@users.noreply.github.com>
2025-06-15 01:44:21 +0530

    Implementing Two Sum Using HashMap

---
+++ HashMap/TwoSum.java
@@ -0,0 +1,21 @@
+/*Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].*/
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int diff = target - nums[i];
+            if(map.containsKey(diff)){
+                return new int[] {map.get(diff), i};
+            }else{
+                map.put(nums[i], i);
+            }
+        }
+        //return null;
+        throw new IllegalArgumentException("No match found");
+    }
+}
