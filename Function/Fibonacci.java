25b3bb73cc6349210ffe2193d57e2c68ca0e3eff
Pratyush Gupta <164205316+pratyushgupta2396@users.noreply.github.com>
2025-06-17 01:25:47 +0530

    Implementing Fibonacci Series

---
+++ Function/Fibonacci.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main
+{
+
+	public static void fibo(int n){
+		int a = 0;
+		int b = 1;
+		for(int i = 1; i <= n; i++){
+			System.out.print(a + " ");
+			int c = a + b;
+			a = b;
+			b = c;
+		}
+	}
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		//your code here
+		Scanner sc = new Scanner(System.in);
+		int n = 10;
+		fibo(n);
+	}
+}
