0468ebdb0fc636f29395cc84da2b92b0666a94e5
Pratyush Gupta <164205316+pratyushgupta2396@users.noreply.github.com>
2025-06-25 17:45:33 +0530

    Implementing Sum of Binary Tree

---
+++ Binary Search Tree/SumofBinaryTree.java
@@ -0,0 +1,27 @@
+/*
+// A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+*/
+
+class Solution {
+    // Function to return sum of all nodes of a binary tree
+    static int sumBT(Node root) {
+        // Your code here
+        if(root == null) return 0;
+        int leftSum = sumBT(root.left);
+        int rightSum = sumBT(root.right);
+        int sum = root.data + leftSum + rightSum;
+        return sum;
+        
+    }
+}
