c39ac7701f1a95b5fb1ab53dc944b3c135d5d277
Pratyush Gupta <164205316+pratyushgupta2396@users.noreply.github.com>
2025-06-25 17:44:49 +0530

    Implementing Size Of Binary Tree

---
+++ Binary Search Tree/SizeOfBinaryTree.java
@@ -0,0 +1,27 @@
+/*
+
+Definition for Binary Tree Node
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data)
+    {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+*/
+
+class Solution {
+    public static int getSize(Node node) {
+        // code here
+        if(node == null) return 0;
+        int ls = getSize(node.left);
+        int rs = getSize(node.right);
+        return ls + rs + 1;
+    }
+}
