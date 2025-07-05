0e71e65a27b169ba75ae95b6684215639159c037
Pratyush Gupta <164205316+pratyushgupta2396@users.noreply.github.com>
2025-07-05 19:17:31 +0530

    Implementing Lucky Integer In An Array

---
+++ HashMap/LuckyIntegerInAnArray.java
@@ -0,0 +1,29 @@
+/*Example 1:
+
+Input: arr = [2,2,3,4]
+Output: 2
+Explanation: The only lucky number in the array is 2 because frequency[2] == 2.
+Example 2:
+
+Input: arr = [1,2,2,3,3,3]
+Output: 3
+Explanation: 1, 2 and 3 are all lucky numbers, return the largest of them.
+*/
+class Solution {
+    public int findLucky(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int n : arr) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+
+        int luckyInteger = -1;
+
+        for (int key : map.keySet()) {
+            if (map.get(key) == key) {
+                luckyInteger = key; 
+            }
+        }
+        return luckyInteger;  
+    }
+}
