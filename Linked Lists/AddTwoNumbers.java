0056ab95cd3fdee0ad2aa4aeac7d1ea9053ededb
Pratyush Gupta <164205316+pratyushgupta2396@users.noreply.github.com>
2025-07-07 21:38:36 +0530

    Implementing Add Two Numbers

--- Linked Lists/AddTwoNumbers.java
+++ Linked Lists/AddTwoNumbers.java
@@ -12,7 +12,7 @@
 /*Input: l1 = [2,4,3], l2 = [5,6,4]
 Output: [7,0,8]
 Explanation: 342 + 465 = 807.*/
-
+// Add Two Nnumbers
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummyHead = new ListNode(0); 
