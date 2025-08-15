52c1892af87c7a513c6180412450f886cf33629c
Pratyush Gupta <164205316+pratyushgupta2396@users.noreply.github.com>
2025-08-15 12:46:19 +0530

    Implementing Product Of Array Except Self

--- Array/ProductOfArrayExceptSelf.java
+++ Array/ProductOfArrayExceptSelf.java
@@ -11,15 +11,22 @@ Output: [24,12,8,6]
 */
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int [] result = new int[nums.length];
+        int [] left = new int[nums.length];
+        int [] right = new int[nums.length];
+
+        left[0] = 1;
+        for(int i = 1; i < nums.length; i++){
+        left[i] = left[i-1] * nums[i-1];
+        }
+
+        right[nums.length -1] = 1;
+        for(int i = nums.length -2; i >= 0; i--){
+        right[i] = right[i+1] * nums[i+1];
+        }
+
+        int[] result = new int[nums.length];
         for(int i = 0; i < nums.length; i++){
-            int product = 1;
-            for(int j = 0; j < nums.length; j++){
-                if(j != i){
-                    product = product * nums[j];
-                }
-            }
-            result[i] = product;
+        result[i] = left[i] * right[i];
         }
         return result;
     }
