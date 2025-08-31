36c25a639a19a252f507b2ed84296c0882c84443
Pratyush Gupta <164205316+pratyushgupta2396@users.noreply.github.com>
2025-09-01 01:04:09 +0530

    Implementing Prime Numbers Range

---
+++ Prime/PrimeNumbersRange.java
@@ -0,0 +1,28 @@
+/*
+  Prime numbers between 1 to 100 are : 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97 
+*/
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		//your code here
+		System.out.print("Prime numbers between 1 to 100 are : ");
+		for(int j = 2; j <= 100; j++){
+			boolean isPrime = true;
+
+			for(int i = 2; i <= Math.sqrt(j); i++){
+				if(j % i == 0){
+					isPrime = false;
+					break;
+				}
+			}
+			if(isPrime){
+				System.out.print(j + " ");
+			}
+		}
+	}
+}
