8b6d6247fb4ea398d814b64bbf7105f3b662e713
Pratyush Gupta <164205316+pratyushgupta2396@users.noreply.github.com>
2025-09-02 00:51:17 +0530

    Implementing Reverse a String

---
+++ Recursion/ReverseString.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main
+{
+	public static void printReverse(String str, int idx){
+		if(idx == 0){
+			System.out.print(str.charAt(idx));
+			return;
+		}
+		System.out.print(str.charAt(idx));
+		printReverse(str, idx-1);
+	}
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		//your code here
+		String str = "abcd";
+		printReverse(str, str.length() -1);
+	}
+}
