dac7a6fe28408daa9f848ec029f5bef1ba4d0499
Pratyush Gupta <164205316+pratyushgupta2396@users.noreply.github.com>
2025-09-09 22:27:07 +0530

    Implementing Duplicates Array

--- Array/FindDuplicatesArray.java
+++ Array/FindDuplicatesArray.java
@@ -10,7 +10,7 @@ class Solution {
     public List<Integer> findDuplicates(int[] nums) {
         List<Integer> result = new ArrayList<>();
         Set<Integer> set = new HashSet<>();
-            for(int i = 0; i < nums.length; i++){
+            for(int i = 0; i < nums.length; i++){ 
                 if(set.contains(nums[i])){
                     result.add(nums[i]);
                 }else{
