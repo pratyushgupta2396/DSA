3334ea0903316065ee29915ed8504b5741bd399e
Pratyush Gupta <164205316+pratyushgupta2396@users.noreply.github.com>
2025-09-14 19:45:27 +0530

    Implementing Bubble Sort

--- Searching & Sorting/BubbleSort.java
+++ Searching & Sorting/BubbleSort.java
@@ -7,7 +7,7 @@ public class Main {
         int N = sc.nextInt();
 
         int arr[] = new int[N];
-
+ 
         // Input of array
         for (int i = 0; i < N; i++) {
             arr[i] = sc.nextInt();
