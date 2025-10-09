5cc3c49f031cca9ab67bb08fce44a2242e5e2c3d
Pratyush Gupta <164205316+pratyushgupta2396@users.noreply.github.com>
2025-10-09 13:49:25 +0530

    Implementing Stock Buy & Sell 3 At most Two Transactions

---
+++ Array/StockBuy&Sell3.java
@@ -0,0 +1,29 @@
+/*
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+Find the maximum profit you can achieve. You may complete at most two transactions.
+
+Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+
+Example 1:
+Input: prices = [3,3,5,0,0,3,1,4]
+Output: 6
+Explanation: Buy on day 4 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+Then buy on day 7 (price = 1) and sell on day 8 (price = 4), profit = 4-1 = 3.
+*/
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buy1 = -prices[0];
+        int sell1 = 0;
+        int buy2 = -prices[0];
+        int sell2 = 0;
+
+        for(int i = 1; i < prices.length; i++){
+            int price = prices[i];
+            buy1 = Math.max(buy1, -price);
+            sell1 = Math.max(sell1, buy1 + price);
+            buy2 = Math.max(buy2, sell1 - price);
+            sell2 = Math.max(sell2, buy2 + price);  
+        }
+        return sell2;
+    }
+}
