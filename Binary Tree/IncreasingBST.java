e6b77114cf0697a085464f03c9c6f590a10c1c9e
Pratyush Gupta <164205316+pratyushgupta2396@users.noreply.github.com>
2025-10-24 23:06:07 +0530

    Implementing Increasing BST

--- Binary Tree/IncreasingBST.java
+++ Binary Tree/IncreasingBST.java
@@ -2,7 +2,7 @@
 Output: [1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]
 */ 
 /** 
- * Definition for a binary tree node.
+ * Definition for a binary tree node. 
  * public class TreeNode {
  *     int val;
  *     TreeNode left;
