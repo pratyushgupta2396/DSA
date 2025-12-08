9656ef3cead069e85d50ff1da1207a605705b2e4
Pratyush Gupta <164205316+pratyushgupta2396@users.noreply.github.com>
2025-12-08 16:17:31 +0530

    Implementing Extra Brackets

--- Stack/ExtraBrackets.java
+++ Stack/ExtraBrackets.java
@@ -1,42 +1,35 @@
-  import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+/*
+Example 1
+Input
+((a + b) + (c + d))
+Output
+false
 
-
+Example 2
+Input
+(a + b) + ((c + d))
+Output
+true
+*/
 class Solution {
     public boolean ExtraBrackets(String exp) {
         // Write your code here
-        Stack<Character> st = new Stack<>();
-        for(int i = 0; i < exp.length(); i++){
-            char ch = exp.charAt(i);
-            if(ch == '('){
-                st.push(ch);
-            }else if(ch == '+' || ch == '-' || ch == '*' || ch == '%' || ch == '*'){
-                st.push(ch);
-            }else if(ch == ')'){
-                if(st.peek() == '('){
-                    return true;
-                }
+       Stack<Character> st = new Stack<>();
+       for(int i = 0; i < exp.length(); i++){
+        char ch = exp.charAt(i);
+        if(ch == ')'){
+            if(st.peek() == '('){
+                return true;
+            }else{
                 while(st.peek() != '('){
                     st.pop();
                 }
                 st.pop();
             }
+        }else{
+            st.push(ch);
         }
-        return false;
-       
-    }
-}
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String exp = br.readLine();
-	    Solution Obj = new Solution();
-        if(Obj.ExtraBrackets(exp))
-            System.out.println("true");
-        else
-            System.out.println("false");
+       }
+       return false;
     }
 }
