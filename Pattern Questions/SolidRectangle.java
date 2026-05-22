af036502ce840a34158a5a9176d08f29c53e9c7d
Pratyush Gupta <164205316+pratyushgupta2396@users.noreply.github.com>
2026-05-22 16:30:29 +0530

    Implementing Solid Rectangle Pattern

---
+++ Pattern Questions/SolidRectangle.java
@@ -0,0 +1,28 @@
+/*
+Print the pattern 
+      *****
+      *****
+      *****
+      *****
+*/
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		//your code here
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		for(int i = 1; i <= n; i++){
+			for(int j = 1; j <= m; j++){
+				System.out.print(" * ");
+			}
+		   System.out.println();
+		}
+	}
+}
