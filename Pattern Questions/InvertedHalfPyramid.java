b5f6fc1c32bfab6b16ea57df10aeed1ae13cae9e
Pratyush Gupta <164205316+pratyushgupta2396@users.noreply.github.com>
2026-05-23 20:05:50 +0530

    Implementing Inverted Half Pyramid Pattern

---
+++ Pattern Questions/InvertedHalfPyramid.java
@@ -0,0 +1,35 @@
+/*
+Print the pattern
+      *
+     **
+    ***
+   ****
+  *****
+ ******
+*******
+
+*/
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		//your code here
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i = 1; i <= n; i++){
+			for(int j = 1; j <= n-i; j++){
+				System.out.print(" ");
+			}
+			for(int j = 1; j <= i; j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
