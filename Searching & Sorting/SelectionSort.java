f3ca8cfd2d4073b5c62fda548a25f98024135881
Pratyush Gupta <164205316+pratyushgupta2396@users.noreply.github.com>
2026-05-26 12:25:12 +0530

    Implementing Selection Sort

--- Searching & Sorting/SelectionSort.java
+++ Searching & Sorting/SelectionSort.java
@@ -1,38 +1,28 @@
-   import java.util.*;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
-    public static int[] SelectionSort(int[] arr) {
-        // Write your code here
-        for(int itr = 0; itr < arr.length-1; itr++){
-            int st = itr;
-            int end = arr.length-1;
-            int minEle = Integer.MAX_VALUE;
-            int minIdx = -1;
-
-            for(int i = st; i <= end; i++){
-                if(minEle > arr[i]){
-                    minEle = arr[i];
-                    minIdx = i;
-                }
-            }
-            int temp = arr[st];
-            arr[st] = arr[minIdx];
-            arr[minIdx] = temp;
-        }
-        return arr;
-
-    }
-    
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-
-        for(int i = 0; i < n; i++)
-            arr[i] = sc.nextInt();
-        int[] ans = SelectionSort(arr);
-
-        for(int a : ans)
-            System.out.print(a + " ");
-    }
+public class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		//your code here
+		int [] arr = {3,5,4,2,1};
+		int n = arr.length;
+		
+			for(int i = 0; i < n; i++){
+				int smallest = i;
+				for(int j = i+1; j < n; j++){
+					if(arr[j] < arr[smallest]){
+						smallest = j;
+					}
+				}
+				int temp = arr[smallest];
+				arr[smallest] = arr[i];
+				arr[i] = temp;
+			}
+				for(int i = 0; i < n; i++){
+			System.out.print(arr[i] + " ");
+		}
+	}
 }
