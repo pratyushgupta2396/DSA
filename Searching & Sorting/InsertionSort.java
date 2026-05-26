cd3ee4f099ec208a133094bf06a606006094af56
Pratyush Gupta <164205316+pratyushgupta2396@users.noreply.github.com>
2026-05-26 12:44:07 +0530

    Implementing Insertion Sort

--- Searching & Sorting/InsertionSort.java
+++ Searching & Sorting/InsertionSort.java
@@ -1,31 +1,27 @@
- import java.util.*;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n= sc.nextInt();
-        int array[] = new int[n];
-
-        for(int i=0; i<n; i++){
-            array[i]= sc.nextInt(); 
-        }
-        
-        insertionSort(array,n);
-    }
-	 static void insertionSort(int arr[], int n)
-    {
-        //Write your code here
-        for(int itr = 1; itr < arr.length; itr++){
-            int i = itr;
-            while(i >= 1 && arr[i-1] > arr[i]){
-                int temp = arr[i];
-                arr[i] = arr[i-1];
-                arr[i-1] = temp;
-                i--;
-            }
-        }
-        for(int i = 0;  i < arr.length; i++){
-            System.out.print(arr[i] + " ");
-        }
-    }
+public class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		//your code here
+		int [] arr = {3,5,4,2,1};
+		int n = arr.length;
+		  
+			for(int i = 1; i < n; i++){
+				int curr = arr[i];
+				int j = i-1;
+				while(j >= 0 && curr < arr[j]){
+					arr[j+1] = arr[j];
+					j--;
+				}
+				
+				arr[j+1] = curr;
+			}	
+				for(int i = 0; i < n; i++){
+			System.out.print(arr[i] + " ");
+		}
+	}
 }
