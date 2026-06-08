4009502cb444c09b8a519a866df12e056c5009df
Pratyush Gupta <164205316+pratyushgupta2396@users.noreply.github.com>
2026-06-08 20:10:56 +0530

    Implementing Add Two Integers

---
+++ Array/AddTwoIntegers.java
@@ -0,0 +1,13 @@
+/*
+Given two integers num1 and num2, return the sum of the two integers.
+ Example 1:
+Input: num1 = 12, num2 = 5
+Output: 17
+
+Explanation: num1 is 12, num2 is 5, and their sum is 12 + 5 = 17, so 17 is returned.
+*/
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1+num2;
+    }
+}
