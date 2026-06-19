27409482436b45e4c35dbb25b70fe85a5067814a
Pratyush Gupta <164205316+pratyushgupta2396@users.noreply.github.com>
2026-06-19 23:28:44 +0530

    Implementing Pivot Index

--- Array/PivotIndex.java
+++ Array/PivotIndex.java
@@ -9,7 +9,7 @@ Return the leftmost pivot index. If no such index exists, return -1.
 Example 1:
 Input: nums = [1,7,3,6,5,6]
 Output: 3
-
+ 
 Explanation:
 The pivot index is 3.
 Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
