9f6ce7f18d828c4c9ca5b3c4e24d49bf0330180b
Pratyush Gupta <164205316+pratyushgupta2396@users.noreply.github.com>
2026-07-17 23:12:25 +0530

    Implementing Fibonacci

--- Function/Fibonacci.java
+++ Function/Fibonacci.java
@@ -8,7 +8,7 @@ public class Main
 	public static void fibo(int n){
 		int a = 0;  
 		int b = 1;
-		for(int i = 1; i <= n; i++){
+		for(int i = 1; i <= n; i++){ 
 			System.out.print(a + " ");
 			int c = a + b; 
 			a = b;
